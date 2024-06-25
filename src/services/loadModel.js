bd64fab6bb209558dba8ae8998af8af90358f217
eugeniusms <eugeniusms@gmail.com>
2024-06-25 16:59:31 +0700

    fix: modelUrl

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,16 @@
 const tf = require("@tensorflow/tfjs-node");
 
 async function loadModel() {
-  return tf.loadGraphModel(process.env.MODEL_URL);
+  const modelUrl =
+    "https://storage.googleapis.com/model-storage-production-bucket/model-in-prod/model.json";
+
+  try {
+    const model = await tf.loadGraphModel(modelUrl);
+    return model;
+  } catch (error) {
+    console.error("Error loading the model:", error);
+    throw error;
+  }
 }
 
 module.exports = loadModel;
